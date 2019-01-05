1efc95c2345903963e5d0513d0a1c3955ff46afc
Isaiah Weaver <isaiahweaver@protonmail.com>
2019-01-04 20:54:55 -0500

    added another listing

--- static/js/components/ListingsCard.jsx
+++ static/js/components/ListingsCard.jsx
@@ -4,9 +4,9 @@ function ListingsCard (props){
   return(
     <div className="listings-card">
         <img src={props.listing.imgUrl}/>
-        <p>Area: Towson, MD</p>
+        <p>Area: {props.listing.area}</p>
         <p>Long Term Storage: $10 per week per bag</p>
-        <p>Short Term Storage: $10 per day per</p>
+        <p>Short Term Storage: $10 per day per day</p>
         <p>Phone: {props.listing.phone}</p>
         <p>Email: {props.listing.email}</p>
 
